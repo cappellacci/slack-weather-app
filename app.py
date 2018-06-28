ea52b51b055806974abef8e489aed6a048397aed
Ben Cappellacci <ben.cappellacci@gmail.com>
2018-06-27 20:03:19 -0400

    adding weather api service

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from flask import Flask, jsonify, request
 
 
@@ -12,11 +13,17 @@ def home():
 @app.route('/slack/weather', methods=['POST'])
 def weather():
     location = request.form["text"]
-    message = 'Its gon rain in {location}'.format(location=location)
+    weather = get_weather(location)
+    message = 'Its gonna be {weather} in {location}'.format(location=location, weather=weather)
     return jsonify({
         'response_type': 'in_channel',
         'text': message 
     })
+
+def get_weather(location):
+    payload = {'q': location, 'APPID': os.environ.get('OPEN_WEATHER_MAP_ACCESS_TOKEN', '0')}
+    r = requests.get('https://api.openweathermap.org/data/2.5/weather', params=payload)
+    return r.json().get('weather')[0].get('main')
 
 if __name__ == '__main__':
     app.run(debug=True,
